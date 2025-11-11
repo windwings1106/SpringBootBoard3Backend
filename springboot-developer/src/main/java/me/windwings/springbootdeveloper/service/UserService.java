4016021cf772f305909180c1c72d1568046bfcf0
windwings1106 <windwings1106@gmail.com>
2025-11-11 15:52:25 +0900

    09

---
+++ springboot-developer/src/main/java/me/windwings/springbootdeveloper/service/UserService.java
@@ -0,0 +1,23 @@
+package me.windwings.springbootdeveloper.service;
+
+import ch.qos.logback.core.pattern.color.BoldCyanCompositeConverter;
+import lombok.RequiredArgsConstructor;
+import me.windwings.springbootdeveloper.domain.User;
+import me.windwings.springbootdeveloper.dto.AddUserRequest;
+import me.windwings.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
