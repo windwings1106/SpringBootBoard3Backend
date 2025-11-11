67207d9d3f138b0dd352ba0cfd405a19bd14c393
windwings1106 <windwings1106@gmail.com>
2025-11-11 11:06:00 +0900

    04

--- springboot-developer/src/main/java/me/windwings/springbootdeveloper/TestController.java
+++ springboot-developer/src/main/java/me/windwings/springbootdeveloper/TestController.java
@@ -1,12 +1,19 @@
 package me.windwings.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+    @Autowired
+    TestService testService;
+
     @GetMapping("test")
-    public String test() {
-        return "Hello, worlds!";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
